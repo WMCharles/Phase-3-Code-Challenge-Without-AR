8930a0cf4b83b9f1fab92e680fe7f078b99b4e17
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-30 13:09:53 +0300

    add art && mag

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,6 +6,16 @@ class Author
         @name = name
         @articles = []  
         @magazines = []
+    end
+
+    # methods for adding author's articles and magazines independently
+    def add_art(article)
+        @articles << article.title
+        article.author = self
+    end
   
+    def add_mag(magazine)
+        @magazines << magazine 
+        magazine.author = self
     end
 end
