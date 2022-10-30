c92f13e3872b14f75d7ee3fa7c3d31fc3035555f
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-30 13:28:52 +0300

    add author class

--- solution.rb
+++ solution.rb
@@ -1,1 +1,43 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Author
+    attr_reader :name
+    attr_accessor :magazine
+  
+    def initialize(name)
+        @name = name
+        @articles = []  
+        @magazines = []
+    end
+
+    # methods for adding author's articles and magazines independently
+    def add_art(article)
+        @articles << article.title
+        article.author = self
+    end
+  
+    def add_mag(magazine)
+        @magazines << magazine 
+        magazine.author = self
+    end
+
+    # article && magazine methods
+    def articles
+        @articles
+    end
+  
+    def magazines
+        @magazines
+    end
+
+    # add_articles and topic_areas method
+    def add_article (magazine, title)
+        article = Article.new(self, magazine, title)
+        @articles << article
+        magazine.add_article(article)
+    end
+  
+    def topic_areas
+        topics = @magazines.map { |mag| mag.category}
+        topics.uniq
+    end
+end
